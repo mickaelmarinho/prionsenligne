ac7c25279ae2ac85510d2980bf0ecc629009b2eb
Mickaël Marinho <mickael_la_carafa@hotmail.fr>
2026-05-11 17:58:21 +0200

    Greg : décalage à gauche du FAB chapelet seulement quand le FAB est présent

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache les pages principales pour consultation hors-ligne
 ═══════════════════════════════════════════════ */
 
-const CACHE_NAME = 'pel-v65';
+const CACHE_NAME = 'pel-v66';
 const PRECACHE = [
   '/',
   '/index.html',
